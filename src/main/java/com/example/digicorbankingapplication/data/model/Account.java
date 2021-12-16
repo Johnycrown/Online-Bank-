0c5acc8e3f764fc8727408ec3dd8d4f220e637a7
Johnycrown <oladejijohn60@gmail.com>
2021-12-16 22:42:01 +0100

    modified

--- src/main/java/com/example/digicorbankingapplication/data/model/Account.java
+++ src/main/java/com/example/digicorbankingapplication/data/model/Account.java
@@ -11,6 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 public class Account {
     @Id
+
     private String accountNumber;
     private  String accountName;
     private String accountPassword;
